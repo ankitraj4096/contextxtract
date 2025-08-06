c77674237dac9daf06f4ff032df364643de5c762
vanimakhija <makhijavani2005@gmail.com>
2025-08-06 23:42:20 +0530

    added spaces n saving to sanjeevani policy

--- pdftotext-folder/test_runner.py
+++ pdftotext-folder/test_runner.py
@@ -1,10 +1,9 @@
-
 import asyncio
 from hackrx_main_simple import process_document_from_file
 
 async def main():
-    file_path = "C:/Users/Vans/Music/hackrx-llm-system/Arogya Sanjeevani Policy.pdf"
+    file_path = (r"C:\Users\Vans\Music\hackrx-llm-system\contextxtract\pdftotext-folder\Arogya Sanjeevani Policy.pdf")
+
     await process_document_from_file(file_path)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
